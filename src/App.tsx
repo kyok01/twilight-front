20fa7fa293e2ecbb759372a7e5010b926ca5bc47
kyok01 <kazutakayokoils@gmail.com>
2023-06-02 18:07:45 +0900

    add steps

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import "./App.css";
 import { AddressReqForm } from "./components/AddressReqForm";
+import { Steps } from "./components/Steps";
 
 function App() {
   return (
-    <div className="bg-neutral min-h-screen">
+    <div className="bg-neutral min-h-screen flex flex-col items-center">
       <div className="navbar bg-base-100 flex justify-center">
         <div>
 
         <img className="max-h-10" src="/twilight-logo.png"/>
         </div>
         <div>
-
         <a className="btn btn-ghost normal-case text-2xl text-primary" href="/">
           Twilight
         </a>
         </div>
       </div>
+      <Steps />
+      <h1 className="text-2xl text-secondary font-bold">STEP1: GET ADDRESS</h1>
       <AddressReqForm />
     </div>
   );
---
+++ src/components/Steps.tsx
@@ -0,0 +1,11 @@
+
+export function Steps() {
+    return (
+        <ul className="steps steps-vertical lg:steps-horizontal">
+  <li className="step step-primary">Register</li>
+  <li className="step step-primary">Choose plan</li>
+  <li className="step">Purchase</li>
+  <li className="step">Receive Product</li>
+</ul>
+    )
+}
