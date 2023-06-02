82a9da01add1ed763e4800dcfe2da9bff37c0207
kyok01 <kazutakayokoils@gmail.com>
2023-06-02 17:59:56 +0900

    add nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import './App.css';
-import { AddressReqForm } from './components/AddressReqForm';
+import "./App.css";
+import { AddressReqForm } from "./components/AddressReqForm";
 
 function App() {
   return (
     <div className="bg-neutral min-h-screen">
-      <h1 className="text-3xl font-bold text-primary">
-    Twilight
-  </h1>
+      <div className="navbar bg-base-100 flex justify-center">
+        <div>
+
+        <img className="max-h-10" src="/twilight-logo.png"/>
+        </div>
+        <div>
+
+        <a className="btn btn-ghost normal-case text-2xl text-primary" href="/">
+          Twilight
+        </a>
+        </div>
+      </div>
       <AddressReqForm />
     </div>
   );
