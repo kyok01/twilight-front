d0fc8d68909ab03ddc1fdbfdf72f17298367ca3a
kyok01 <kazutakayokoils@gmail.com>
2023-06-03 10:49:20 +0900

    update: the way to make contract object

--- src/components/AddressReqForm/useAddressReqForm.tsx
+++ src/components/AddressReqForm/useAddressReqForm.tsx
@@ -2,22 +2,19 @@ import { ChangeEvent, useCallback, useEffect, useState } from "react";
 import { contractMaker } from "../../hooks/useProvider";
 
 export const useAddressReqForm = () => {
-    useEffect(()=>{
-        const {contract} = contractMaker();
-        setContract(contract);
-    },[])
 
     const [inputValue, setInputValue] = useState<string>('');
     const [address, setAddress] = useState('')
-    const [contract, setContract] = useState<any>()
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value)
   }
 
   const handleClick = useCallback(async() => {
+    const {contract} = contractMaker();
     const address = await contract.methods.getAddress('0xa4E1040705Cb9434D97A295079fe0442eE571456', inputValue).call();
     console.log(address);
     setAddress(address);
+    return;
   },[])
 
   return {inputValue, setInputValue, address, setAddress, handleClick, handleChange}
